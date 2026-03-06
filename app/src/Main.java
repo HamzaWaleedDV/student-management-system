a758578b5b3d886e9a3868356d689d168c0cac9b
Omar Kamal <omarahmed.9825@azhar.edu.eg>
2026-03-06 02:13:06 +0200

    Omar

--- app/src/Main.java
+++ app/src/Main.java
@@ -48,5 +48,5 @@ public class Main {
     
     public static void main(String[] args) {
 
-    }
+    }   
 }
--- app/src/Student.java
+++ app/src/Student.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Student {
@@ -8,7 +9,7 @@ public class Student {
     private int _seatNumber;
     private String _firstName;
     private String _lastName;
-    
+
     public Student(String firstName, String lastName, int age, int grade, int seatNumber) {
         _firstName = firstName;
         _lastName = lastName;
@@ -62,17 +63,88 @@ public class Student {
         return _lastName;
     }
 
-    public void set_lastName(String _lastName) { 
+    public void set_lastName(String _lastName) {
         this._lastName = _lastName;
     }
 
     public static void createStudent() {
         Student newStudent = new Student(Util.readString("Enter first name: "), Util.readString("Enter last name: "),
-        Util.readValidNumber("Enter you age: "), Util.readValidNumber("Enter your grade: "), Util.readValidNumber("Enter your seat number: "));
+                Util.readValidNumber("Enter you age: "), Util.readValidNumber("Enter your grade: "),
+                Util.readValidNumber("Enter your seat number: "));
 
         Main.students.add(newStudent);
     }
 
-    
-}
+    public static void updateStudent() {
+        int index = findStudent(Util.readValidNumber("Please Enter Student ID: "));
+        Scanner scanner = new Scanner(System.in);
+        Main.students.get(index).printStudentCard();
+        String userInput = Util
+                .readString("What do you want to update? (First Name, Last Name, Age, Grade, Seat Number)");
+        switch (userInput) {
+            case "First Name":
+                String firstName = Util.readString("Please enter new First Name: ");
+                Main.students.get(index).set_firstName(firstName);
+                break;
+            case "Last Name":
+                String lastname = Util.readString("Please enter new Last Name: ");
+                Main.students.get(index).set_lastName(lastname);
+            case "Age":
+                int age = Util.readValidNumber("Please Enter new age: ");
+                Main.students.get(index).set_age(age);
+            case "Grade":
+                int grade = Util.readValidNumber("Please Enter new grade: ");
+                Main.students.get(index).set_grade(grade);
+            case "Seat Number":
+                int seatNumber = Util.readValidNumber("Please Enter new Seat Number: ");
+                Main.students.get(index).set_seatNumber(seatNumber);
+            default:
+                System.out.println("Invalid input, Please Try again.");
+                break;
+        }
+    }
 
+    // public void updateStudent() {
+    //     String userInput = Util
+    //             .readString("What do you want to update? (First Name, Last Name, Age, Grade, Seat Number)");
+    //     switch (userInput) {
+    //         case "First Name":
+    //             String firstName = Util.readString("Please enter new First Name: ");
+    //             set_firstName(firstName);
+    //             break;
+    //         case "Last Name":
+    //             String lastname = Util.readString("Please enter new Last Name: ");
+    //             set_lastName(lastname);
+    //         case "Age":
+    //             int age = Util.readValidNumber("Please Enter new age: ");
+    //             set_age(age);
+    //         case "Grade":
+    //             int grade = Util.readValidNumber("Please Enter new grade: ");
+    //             set_grade(grade);
+    //         case "Seat Number":
+    //             int seatNumber = Util.readValidNumber("Please Enter new Seat Number: ");
+    //             set_seatNumber(seatNumber);
+    //         default:
+    //             System.out.println("Invalid input, Please Try again.");
+    //             break;
+    //     }
+    // }
+
+    public static void deleteStudent() {
+        int index = findStudent(Util.readValidNumber("Please Enter Student ID: "));
+        Scanner scanner = new Scanner(System.in);
+        Main.students.get(index).printStudentCard();
+        String userInput = Util
+                .readString("Are you sure you wnat to delete this student? (Y, N)");
+        switch (userInput) {
+            case "Y":
+                Main.students.remove(index);
+                break;
+            default:
+                System.out.println("Nothing have changed.");
+                break;
+        }
+        
+    }
+
+}
